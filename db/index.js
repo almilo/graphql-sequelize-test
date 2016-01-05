886b361b79f009f23f0c2ff0ee64c4fda2d3f63b
Alberto Mijares <alberto.mijares@edorasware.com>
2016-01-05 23:42:39 +0100

    (feat): implement relationships

--- db/index.js
+++ db/index.js
@@ -1,4 +1,11 @@
 import {Db} from './db';
+import {Address} from './address';
+import {Person} from './person';
+import {Customer} from './customer';
 import {createMockData} from './mock-data';
+
+Address.belongsToMany(Person, {through: 'address_person'});
+Address.belongsToMany(Customer, {through: 'address_customer'});
+Person.belongsTo(Customer);
 
 Db.sync({force: true}).then(createMockData);
--- db/mock-data.js
+++ db/mock-data.js
@@ -5,11 +5,31 @@ import {Address} from './address';
 import {Customer} from './customer';
 
 export function createMockData() {
-    _.times(10, _ => person());
-    _.times(10, _ => address());
-    _.times(10, _ => customer());
+    Promise.all(_.times(10, createMockAddress)).then(addresses => {
+        Promise.all(_.times(10, createMockPerson)).then(people => {
+            Promise.all(_.times(10, createMockCustomer)).then(customers => {
+                people.forEach(person => {
+                    person.setCustomer(random(customers));
 
-    function person() {
+                    const address = random(addresses);
+                    if (address) {
+                        address.addPerson(person);
+                    }
+                });
+
+                customers.forEach(customer => {
+                    const address = random(addresses);
+                    if (address) {
+                        address.addCustomer(customer);
+                    }
+                });
+            });
+        });
+    }).catch(reason => {
+        throw new Error(reason);
+    });
+
+    function createMockPerson() {
         return Person.create({
             salutation: Faker.name.prefix(),
             firstname: Faker.name.firstName(),
@@ -22,7 +42,7 @@ export function createMockData() {
         });
     }
 
-    function address() {
+    function createMockAddress() {
         return Address.create({
             street: Faker.address.streetAddress(),
             city: Faker.address.city(),
@@ -31,11 +51,17 @@ export function createMockData() {
         });
     }
 
-    function customer() {
+    function createMockCustomer() {
         return Customer.create({
             company: Faker.company.companyName(),
             department: Faker.company.bs(),
             website: Faker.internet.url()
         });
+    }
+
+    function random(collection) {
+        const index = Math.floor(Math.random() * collection.length);
+
+        return collection[index] || null;
     }
 }
